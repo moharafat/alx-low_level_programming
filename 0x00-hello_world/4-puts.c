a56f3256b912876641769d1c6eed9b8ae0941c34
moharafat <mohamedamr0001@gmail.com>
2023-10-05 16:38:04 -0700

    z

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - this is the main entry point of the program
+ *main - this is the main entry point of the progrm
  *Description: i will use put not printf
  *Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
