06a699e19c8c56214bd042183fc8315ce55475a8
moharafat <mohamedamr0001@gmail.com>
2023-10-05 15:18:45 -0700

    z

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message with proper grammar.
+ * Return: Always 0 (success)
+ */
 int main(void)
-{ 
-	printf("with proper grammar, but the outcome is a piece of art,"\n);
-	Return(0);
+{
+	    printf("with proper grammar, but the outcome is a piece of art,\n");
+	        return (0);
 }
