2aba82be3b7fcf36d7ca44c7d81f60142f627e05
moharafat <mohamedamr0001@gmail.com>
2023-10-05 15:57:23 -0700

    z

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  *Description: i will use put not printf
  *Return: Always 0 (success)
  */
-int.main(void)
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle,\n);
 	return (0);
