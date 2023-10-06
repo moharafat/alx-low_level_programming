b4c44c11f68d9024c5cba1a9352ba2e0e180f982
moharafat <mohamedamr0001@gmail.com>
2023-10-05 17:27:04 -0700

    z

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry point
  *
- * Description: using sizeof to print the size of the various types
+ * Description: using sizeof to print the size of the various types.
  *
- * Return Always 0 (Sucess)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
