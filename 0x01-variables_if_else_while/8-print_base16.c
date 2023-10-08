429d12f68fedd44f66d835916b6126a7d896248f
moharafat <mohamedamr0001@gmail.com>
2023-10-09 00:39:40 +0300

    shenouda's shortcut

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+
+(char c = '0'; c <= 'F'; c++) {
+        putchar(c);
+}
+
+
+
+
+
+	putchar('\n');
+return (0);
+}
