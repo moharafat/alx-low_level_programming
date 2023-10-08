6e41888c8248c86d40214823454d5d24b9ee3986
moharafat <mohamedamr0001@gmail.com>
2023-10-08 23:39:31 +0300

    lets go

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,14 +10,15 @@ int main(void)
 {
 int a = 97;
 
-while (a >= 97 && a <= 122)	{
+while (a >= 97 && a <= 122)
+
 	if (a != 101 && a != 113)
 
 	{
 	putchar(a);
 	a++;
 	}
-}
+
 	putchar('\n');
 
 return (0);
