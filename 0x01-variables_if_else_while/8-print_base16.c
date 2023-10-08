4c250bb6def9462a08c229c6cee56fa1ce40d38a
moharafat <mohamedamr0001@gmail.com>
2023-10-09 00:50:59 +0300

    him

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: always 0 (Success)
-*/
-int main()
-{
+int main() {
+    // Iterate through digits 0-9 and characters a-f
+    for (int i = 0; i < 16; i++) {
+        // Print digits 0-9
+        putchar(i < 10 ? '0' + i : 'a' + i - 10);
+    }
 
-for (int i = 0; i < 16; i++) {
-		// Print digits 0-9
-		putchar(i < 10 ? '0' + i : 'a' + i - 10);
-}
+    // Print a new line
+    putchar('\n');
 
-
-	putchar('\n');
-return (0);
+    return 0;
 }
