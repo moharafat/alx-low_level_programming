d3b46d35a9725040a3c6e127a9850e4b0cdb5374
moharafat <mohamedamr0001@gmail.com>
2023-10-08 15:31:40 +0300

    totaly alone

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+int a = 97;
+int b = 65;
+while (a >= 97 && a <= 122)
+	{
+	putchar(a);
+	a++;
+	}
+while (b >= 65 && b <= 90)
+	{
+	putchar(b);
+	a++;
+	}
+
+	putchar("\n")
+	
+return (0)
+}
