8115461703999f929ba9dab07ed5dcf44ed42765
moharafat <mohamedamr0001@gmail.com>
2023-10-08 14:59:00 +0300

    lets goooooo

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point   
- * 
+ * main - Entry point
+ *
  * Return: always 0 (Success)
 */
 int main(void)
@@ -10,10 +10,10 @@ int main(void)
 int a = 97;
 
 while (a >= 97 && a <= 122)
-    {
-    putchar(a);
-    a++;
-    }
-    putchar('\n');
+   {
+   putchar(a);
+   a++;
+   }
+   putchar('\n');
 return (0);
 }
