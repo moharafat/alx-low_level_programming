58b3af8cbdb1e7655da200d86255f89e51bbc0ca
moharafat <mohamedamr0001@gmail.com>
2023-10-08 21:15:31 +0300

    same as the 1 before

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+int n = 0;
+
+	while (n >= 0 && n <= 9)
+	{
+	putchar(n + '0');
+	n++;
+	}
+
+	putchar('\n');
+return (0);
+}
