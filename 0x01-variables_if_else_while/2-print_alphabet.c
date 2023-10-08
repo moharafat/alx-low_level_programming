8d77c55a286ff77eb620c7fe76388d94d5f392d5
moharafat <mohamedamr0001@gmail.com>
2023-10-08 13:30:52 +0300

    lets go

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point   
+ * 
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+int a = 97;
+
+while (a < 123) 
+    {
+    int putchar('a');
+    a++;
+    }
+
+}
