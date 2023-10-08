3f7fa904eb8d0b7e69643c5f3722956f31a31c56
moharafat <mohamedamr0001@gmail.com>
2023-10-09 02:11:20 +0300

    pink

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+int digi = 0;
+	while (digi < 9)
+	{
+		putchar (digi + 48);
+
+		if (digi != 9)
+		{	
+			puchchar (,);
+			puchchar ( );
+		}
+		digi++;
+
+	}
+	putchar('\n');
+return (0);
+}
