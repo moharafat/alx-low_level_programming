4b4f025e326ee7fed8b2f3bdfe49ac48aab6b44f
moharafat <mohamedamr0001@gmail.com>
2023-10-08 21:35:47 +0300

    same as task2

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+int a = 122;
+
+while (a >= 97 && a <= 122)
+	{
+	putchar(a);
+	a--;
+	}
+	putchar('\n');
+return (0);
+}
