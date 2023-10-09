c026f8cdca48ae5183260d02bb2bf970ec351728
moharafat <mohamedamr0001@gmail.com>
2023-10-09 03:00:03 +0300

    pink

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,21 +6,20 @@
  * Return: always 0 (Success)
 */
 
+
 int main(void)
 {
+    char letter;
 
-	char letter;
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        if (letter != 'e' && letter != 'q')
+        {
+            putchar(letter);
+        }
+    }
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		{
-		if (letter != 'e' && letter != 'q')
+    putchar('\n');
 
-				putchar(letter);
-
-		}
-
-
-	putchar('\n');
-
-return (0);
+    return (0);
 }
