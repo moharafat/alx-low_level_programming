454df6687c999cb147f62b276c8258b90b3184b8
moharafat <mohamedamr0001@gmail.com>
2023-10-10 16:32:58 -0700

    z

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,11 @@
 void print_alphabet_x10(void)
 
 	int l, c;
-	for (l = 0; l <=9; l++)
-	{	for (c = 'a'; c <= 'z'; c++)
+	for (l = 0; l <= 9; l++)
+	{	
+		for (c = 'a'; c <= 'z'; c++)
 
 			_putchar(c);
+
 	_putchar('\n');
 	}
-
