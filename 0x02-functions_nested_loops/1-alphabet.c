13149877265c77ee98cfa63a94254dcba2230f83
moharafat <mohamedamr0001@gmail.com>
2023-10-10 05:23:00 -0700

    z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include "main.h"
+nclude <stdio.h>
 
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: always 0 (Success)
+ *    */
+int main(void)
+{
+	int a;
+
+	for (a = 97;a >= 97 && a <= 122; a++)
+			{
+					putchar(a);
+						
+			}
+	putchar('\n');
+	return (0);
+}
