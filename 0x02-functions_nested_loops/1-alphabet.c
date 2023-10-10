5ade6e687cebe1f86befb0df3010e409c907c9db
moharafat <mohamedamr0001@gmail.com>
2023-10-10 07:54:42 -0700

    z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,25 @@
-int _putchar(char c)
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: always 0 (Success)
+ *    */
+
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		{
+		putchar(x);
+		}
+
+			{
+			putchar('\n');
+			}
+return (0);
+}
+
