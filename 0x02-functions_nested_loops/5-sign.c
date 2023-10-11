250b01d22c86ab6390998bdb97c571f776939f94
moharafat <mohamedamr0001@gmail.com>
2023-10-10 17:38:10 -0700

    z

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,19 +4,21 @@
  *
  *dsaasd
  * */
+
 int print_sign(int n)
+
 {	
 	if (n >= 1)
 	{
 		_putchar (43);
 		return (1);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		_putchar (48);
 		return (0);
 	}
-	if (n <= 1)
+	else
 	{
 		_putchar (45);
 		return (-1);
