454c0c30f06e9001e4fbb68b6171851e20f7c9be
moharafat <mohamedamr0001@gmail.com>
2023-10-10 18:23:09 -0700

    z

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ **/
+
+int print_last_digit(int n)
+{	
+	int last_digit;
+	if (n < 0)
+		last_digit = -1 *(n % 10);
+	else 
+		last_digit = n % 10;
+
+			_putchar (last_digit + 48);
+			return (last_digit);
+}
