61f4b6d438c8b4133cd972f57670f7d22f1d02a6
moharafat <mohamedamr0001@gmail.com>
2023-10-12 04:36:11 +0300

    s

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of the various types.
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_times_table(int n)
+
+{
+	int number
+
+	if (number <= 15) && (number >= 0)
+
+		for (n = 0; number <= n; number++)
+		{
+			_putchar(48);
+			for 
+
+
+		}
+
+
+
+return = (0)
+
+}
