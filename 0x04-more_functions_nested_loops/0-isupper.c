b991346dd69f57909bbb55c9a50918e19f2788fc
moharafat <mohamedamr0001@gmail.com>
2023-10-12 15:25:19 +0300

    SS

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of the various types.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _isupper(int c)
+{
+
+if (c >= 97 && c <= 122)
+	return (1)
+else
+	return (0)
+}
