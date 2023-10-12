d752afc712f216da34118dc411372f0479466fb3
moharafat <mohamedamr0001@gmail.com>
2023-10-12 17:40:15 +0300

    s

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,11 @@
 
 void print_numbers(void)
 {
-	int x;
+int x;
 
-	for (x = 48; x <= 57; x++)
-	{
-	_putchar(x);
-	}	
-	_putchar('\n');
+for (x = 48; x <= 57; x++)
+{
+_putchar(x);
+}	
+_putchar('\n');
 }
