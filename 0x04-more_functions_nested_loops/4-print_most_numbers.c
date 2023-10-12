09791df4d15fba91702e4bb5efb00dc58357f19c
moharafat <mohamedamr0001@gmail.com>
2023-10-12 21:53:56 +0300

    s

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,9 @@ void print_most_numbers(void)
 {
 	int x;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 48; x <= 57; x++)
 		{
-			if (x == 2) || (x == 4)
+			if (x == 50) || (x == 52)
 				continue;
 			_putchar(x + 48);
 		}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+
+{
+    for ()
+
+
+}
