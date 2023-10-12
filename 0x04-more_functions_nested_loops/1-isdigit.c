021975db9739cf65d822053cc832e7ff9895bb88
moharafat <mohamedamr0001@gmail.com>
2023-10-12 15:44:27 +0300

    s

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -12,7 +11,7 @@ int _isdigit(int c)
 {
 
 if (c >= 48 && c <= 57)
-    return (1);
-else 
-    return (0);
+	return (1);
+else
+	return (0);
 }
