6088c9729c35176aacbed91d275a59f0caca89de
moharafat <mohamedamr0001@gmail.com>
2023-10-12 17:19:47 +0300

    mistakes were made

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+
+{
+
+int c;
+
+	for (c = 0; c >= 48 && c <= 57; c++)
+	{
+		_putcahr(c);
+	}
+		_putchar('\n');
+}
