a28424e708c4fc86e9ad294425433a1bc03b3f3f
moharafat <mohamedamr0001@gmail.com>
2023-10-13 21:13:20 +0300

    s

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,10 +12,10 @@ int main(void)
 	int n;
 
 	for (n = 1; n <= 100; n++)
-	{	
+	{
 		if (n % 3 == 0 && !(n % 5 == 0))
 				printf("Fizz");
-		else if (n % 5 == 0 && !(n % 3 ==0))
+		else if (n % 5 == 0 && !(n % 3 == 0))
 				printf("Buzz");
 		else if (n % 3 == 0 && n % 5 == 0)
 				printf("FizzBuzz");
@@ -26,7 +26,7 @@ int main(void)
 
 		if (n != 100)
 				printf(" ");
-		else 
+		else
 				printf("\n");
 	}
 	return (0);
