ddb4fe1dc59a3490cbfdc5c6973d83d564519e44
moharafat <mohamedamr0001@gmail.com>
2023-10-13 21:50:38 +0300

    s

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,10 +12,10 @@ void print_most_numbers(void)
 
 	for (x = 48; x <= 57; x++)
 	{
-			if (x != 50 && x != 52)
-			{
-			_putchar(x);
-			}
+		if (x != 50 && x != 52)
+		{
+		_putchar(x);
+		}
 	}
 	_putchar('\n');
 
