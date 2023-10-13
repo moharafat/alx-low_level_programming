db94aa7163a962420b3abc3f97bd64d5bb62026e
moharafat <mohamedamr0001@gmail.com>
2023-10-13 21:24:50 +0300

    s

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,8 +17,9 @@ void print_diagonal(int n)
 		for (L = 1; L <= n; L++)
 		{
 			for (space = 1; space < L; space++)
-				_putchar(' ');
+			{	_putchar(' ');
 			_putchar(92);
+			}
 			_putchar('\n');
 		}
 
