ea2ef0bc6efba5ea89e5fb845aee5994175ad27b
moharafat <mohamedamr0001@gmail.com>
2023-10-13 23:17:16 +0300

    s

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,17 +11,17 @@ void more_numbers(void)
 	int num, line, count;
 
 	for (line = 1; line <= 10; line++)
-		{
+	{
 			for (count = 0; count <= 14; count++)
 			{
 				num = count;
 				if (count > 9)
 				{
-					_putchar(1 + 48);
-					num =  count % 10;
+				_putchar(1 + 48);
+				num =  count % 10;
 				}
-					_putchar(num + 48);
+				_putchar(num + 48);
 			}
-				_putchar('\n');
-		}
+			_putchar('\n');
+	}
 }
