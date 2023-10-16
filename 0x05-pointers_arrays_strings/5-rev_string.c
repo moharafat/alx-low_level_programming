0858e833b8f5c2a2fee4fedb62d0f9b31cda2a4e
moharafat <mohamedamr0001@gmail.com>
2023-10-16 23:51:27 +0300

    s

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,13 +7,13 @@
 
 void rev_string(char *s)
 {
-	int l,i;
+	int l, i;
 	char temp;
 
 	for (l = 0; s[l] != '\0'; ++l)
 	;
 
-	for (i = 0; i < 1 /2; i++)
+	for (i = 0; i < 1 / 2; i++)
 	{
 		temp = s[i];
 		s[i] = s[l - 1 - i];
