25fc8facd89496e027b9fe06594275e4239ba656
moharafat <mohamedamr0001@gmail.com>
2023-10-16 23:56:38 +0300

    s

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,16 +8,13 @@
 void print_rev(char *s) {
 	int length = 0;
 
-	// Find the length of the string
 	while (s[length] != '\0') {
 		length++;
 	}
 
-	// Print characters in reverse order
 	for (int i = length - 1; i >= 0; i--) {
 		_putchar(s[i]);
 	}
 
-	// Print a new line
 	_putchar('\n');
 }
