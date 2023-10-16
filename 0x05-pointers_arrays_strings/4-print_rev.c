48d145c44d38e09cf77a14f19a478e06a6215bcd
moharafat <mohamedamr0001@gmail.com>
2023-10-16 21:28:45 +0300

    s

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,14 +5,15 @@
  * Return: Always 0.
  */
 
-void print_rev(char *s) {
-	int length = 0;
+void print_rev(char *s)
+{
+	int len = 0, index;
 
-	while (s[length] != '\0') {
-		length++;
-	}
-	for (int i = length - 1; i >= 0; i--) {
-		_putchar(s[i]);
-	}
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
+
 	_putchar('\n');
 }
