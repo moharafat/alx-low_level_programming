8ea8ee51f7af334ae4b43ceba14fa20eed4f3e34
moharafat <mohamedamr0001@gmail.com>
2023-10-17 00:30:28 +0300

    s

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts_half - function that prints half of a string
+ * @str: string parameter input
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	
