4c1ec3c8c213f343012699c0f80bc7312a478102
moharafat <mohamedamr0001@gmail.com>
2023-10-16 21:25:03 +0300

    s

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev -  function that prints a string, in reverse
+ *@s:
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+int length = 0;
+
+while (s[length] != '\0') {
+	length++
+}
+for (int i = length - 1; i >= 0; i--) {
+	_putchar(s[i]);
+	}
+	_putchar('\n');
+}
