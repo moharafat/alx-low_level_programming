bc1958ee507c81218cdf84d96f1d5c505a090d7c
moharafat <mohamedamr0001@gmail.com>
2023-10-19 17:51:08 +0300

    s

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * *_strncat - check the codefunction that concatenates two strings
  *@dest: 1st input
@@ -11,12 +10,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
-	int n;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; src[j] < n; j++)
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 	dest[i + j] = src[j];
 	}
