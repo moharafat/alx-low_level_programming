4ecdfa4366e8bca2a2a4389a4b841074d7a4f1dc
moharafat <mohamedamr0001@gmail.com>
2023-10-19 15:59:56 +0300

    s

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,21 @@
-dl
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcat - check the codefunction that concatenates two strings
+ *@dest: 1st input
+ *@src: second input
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+
+	for (j = 0; src[j] == '\0'; j++)
+	{
+	dest[i + j] = src[j];
+	}
+	return(dest);
+}
