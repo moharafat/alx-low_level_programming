a2006efa64e2ecc6723edaf1b571f9b4d8551917
moharafat <mohamedamr0001@gmail.com>
2023-10-19 19:29:08 +0300

    s

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strncpy -  function that copies a string
+ *@dest: first input
+ *@src: second input
+ *@n: third input
+ * Return: nothing
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+
+}
