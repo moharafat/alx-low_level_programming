e42a1d82406aa579254fb5693e3f86edc505fc51
moharafat <mohamedamr0001@gmail.com>
2023-10-20 19:03:34 +0300

    s

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,21 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * cap_string - capitalizes all words of a string
+ *@s: string parameter
+ * Return: string s
  */
-char *cap_string(char *)
+char *cap_string(char *s)
 {
+int i;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122  && )
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
 }
+32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125
+or  = " \t\n,;.!?\"(){}";
