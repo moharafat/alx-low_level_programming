8cd7c5fff8df09ee5671d11d180eae0bbeb3af4f
moharafat <mohamedamr0001@gmail.com>
2023-10-21 20:21:24 +0300

    s

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,18 @@
 #include "main.h"
+/**
+ * *leet -  encodes a string into 1337.
+ *@s: string parameter
+ * Return: string s
+ */
+char *leet(char *s)
+{
+char b;
+int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+    {
+        if s[i] = {a,A,e,E,o,O,t,T,l,L}  //only 1 if conditional allowed
+		
+		s[i] = '4';  //subtraction won't work like in the previous task
+    }
+}
