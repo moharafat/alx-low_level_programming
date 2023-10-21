a0d0c98ef912747fc32c663662c0e366124ff9db
moharafat <mohamedamr0001@gmail.com>
2023-10-21 14:04:35 +0300

    s

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,6 +8,11 @@ char *cap_string(char *s)
 {
 int i;
 
+	if (s[0] >= 97 && s[i] <= 122)
+	{
+		s[0] = s[0] - 32;
+	}
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || 
