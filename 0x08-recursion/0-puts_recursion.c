5e1b1fba0788464da267a0c5455eda205b44e3e2
moharafat <mohamedamr0001@gmail.com>
2023-10-25 18:09:53 +0300

    s

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,15 @@
-#
+#include "main.h"
+/**
+ * _puts_recursio - prints a string, followed by a new line
+ *@s: parameter input
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
