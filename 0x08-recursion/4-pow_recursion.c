b710dab545b4282b029902c1545b23d573db3b71
moharafat <mohamedamr0001@gmail.com>
2023-10-26 14:15:29 +0300

    s

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,12 +11,12 @@ int _pow_recursion(int x, int y)
 	{
 	return (-1);
 	}
-	if (y == 0)
+	else if (y == 0)
 	{
 	return (1);
 	}
 
-	if (y == 1)
+	else if (y == 1)
 	return (x);
 
 	return (x ^ y);
