a056ee0f42d3ed7fd0ef7b0c61b9928c88125824
moharafat <mohamedamr0001@gmail.com>
2023-10-26 09:44:35 +0300

    s

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,6 +10,6 @@ int _strlen_recursion(char *s);
 	int length = 0;
 	if (*s == '\0')
 		return (0);
-	 _strlen_recursion 1 + (*s + 1);
-	 return (0);
+	 length = 1 + _strlen_recursion (*s + 1);
+	 return (length);
 }
