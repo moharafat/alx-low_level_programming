ba06f6d1d262f721d0ae804e7c4ecd646a81a598
moharafat <mohamedamr0001@gmail.com>
2023-10-26 13:39:40 +0300

    s

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _pow_recursion - function that returns the value of x raised to the power of y
- *@y: given number parameter
+ *@x: given number parameter
  *@y: given number that is the square
  * Return: Always 0 except wehn y is less than 0 return -1
  */
 int _pow_recursion(int x, int y)
 
 {	if (y < 0)
-		return (-1);
-	
+	return (-1);
+
+	return (x ^ y);
 }
