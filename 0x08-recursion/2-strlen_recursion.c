cbacab764b027a8381b8a62f8da47794131e6775
moharafat <mohamedamr0001@gmail.com>
2023-10-26 10:20:41 +0300

    s

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,14 @@
 int _strlen_recursion(char *s)
 {
 	int length = 0;
+	
 	if (*s == '\0')
 	{
 	return (0);
 	}
-	else 
+	else
 	{
-	length += 1 + _strlen_recursion (s + 1);
+	length += 1 + _strlen_recursion(s + 1);
 	return (length);
 	}
 }
