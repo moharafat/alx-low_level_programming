fafe369d1385771e5d23e9642df6f99d642fcfbf
moharafat <mohamedamr0001@gmail.com>
2023-10-26 19:13:30 +0300

    s

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,16 +16,3 @@ int is_prime_number(int n, int divisor)
     }
     return is_divisible(n, divisor - 1); // Try the next divisor
 }
-
-/**
- * is_prime_number - Returns 1 if the input integer is a prime number, 0 otherwise
- * @n: The number to check for primality
- * Return: 1 if n is prime, 0 otherwise
- */
-int is_prime_number(int n)
-{
-    if (n < 2)
-    {
-        return 0; // 0 and 1 are not prime
-    }
-    return is_divisible(n, n - 1); // Start checking divisibility from n-1
