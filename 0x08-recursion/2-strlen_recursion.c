0b56810a0a0ae88ccf8c542d8212732a7be6cfb7
moharafat <mohamedamr0001@gmail.com>
2023-10-26 09:11:11 +0300

    s

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,7 +7,7 @@
 int _strlen_recursion(char *s);
 {
     // Base case: if the current character is the null terminator, return 0
-    if (s == '\0')
+    if (*s == '\0')
     {
         return 0;
     }
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that returns the value of x raised to the power of y
+ *@y: given number parameter
+ *@y: given number that is the square
+ * Return: Always 0 except wehn y is less than 0 return -1
+ */
+int _pow_recursion(int x, int y)
+
+{	if (y < 0)
+		return (-1);
+	
+}
