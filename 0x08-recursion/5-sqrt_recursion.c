e9366303f652533755626644a412baebbd0c9393
moharafat <mohamedamr0001@gmail.com>
2023-10-27 05:58:06 +0200

    s

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,12 +3,22 @@
  * _sqrt_recursion - returns the natural square root of a number.
  *@n: input number parameter which we will get the square root
  * Return: Always 0
- */
+ */ 
+int x;
+x = 1;
+ 
+int helper(n , x)//function defintion which  could be  int helper(n , 1)
+{
+	if (x == n)
+	return (-1);
+	if (x * x == n)
+	return (x);
+	return helper (n, x + 1);//Return function 
+}
+
+
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	return (-1);
+	
 
-
-// n*n = n{\sqrt {n}}.
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,15 +4,26 @@
  *@n: input parameter
  * Return: Always 0.
  */
+int n;
+int x;
+int helper (n , x) //function def. must begin with bec.the compiler goes from up to down
+{
+	if (n == x)
+	{
+	return (1);
+	}
+
+	if (n % x == 0)
+	{
+	return (0);
+	}
+	if (n == 1)
+	return (0);
+
+	return helper (n, x + 1); //Return function 
+}
+
 int is_prime_number(int n, int divisor)
 {
-    if (divisor == 1)
-    {
-        return 1; // If we've checked all divisors up to 1, it's prime
-    }
-    if (n % divisor == 0)
-    {
-        return 0; // If n is divisible by any divisor, it's not prime
-    }
-    return is_divisible(n, divisor - 1); // Try the next divisor
+return helper (n , x);
 }
