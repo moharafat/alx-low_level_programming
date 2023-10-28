beff02b800aacf57b072852f526c1299c1f79ae5
moharafat <mohamedamr0001@gmail.com>
2023-10-28 10:29:38 +0200

    s

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print the program name + new line
+ *@argc: int
+ *@argv: list
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
