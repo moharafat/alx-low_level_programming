fc438bfdf4fed28fe0b62252ee78a7ab30b4168a
moharafat <mohamedamr0001@gmail.com>
2023-10-28 16:20:26 +0200

    s

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,8 +14,10 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
-			if (c* < 48 || c* > 57)
+		{
+			if (c* < '0' || c* > '9')
 			return (printf("Error\n"), 1);
+		}
 		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
