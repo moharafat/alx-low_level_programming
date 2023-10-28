bfc954bd106ebd7361b4048e17ab6a8aaeb0f2e8
moharafat <mohamedamr0001@gmail.com>
2023-10-28 12:13:41 +0200

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,15 +9,15 @@
 int main(int argc, char *argv[])
 int multiplication;
 {
-    if ( argc != 3)
-    {
-        printf("Error");
-        return (1);
-    }
-    else
-    {
-    multiplication = atoi(argv[1]) * atoi(argv[2]);
-    printf("%d\n", multiplication);
-    return (0);
-    }
+	if ( argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+	multiplication = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", multiplication);
+	}
+	return (0);
 }
