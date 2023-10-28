1bd1b3860d1e035a8fb75db40c32ab876cc0f027
moharafat <mohamedamr0001@gmail.com>
2023-10-28 12:28:17 +0200

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - multiplies two numbers
- *@argc: integer
- *@argv: array of string
- *
- * Return: integer result of the multiplication
- * 1 on error
+ * main - Write a program that multiplies two numbers.
+ *@argc: int
+ *@argv: string
+ * Return: always 0
  */
-
 int main(int argc, char *argv[])
 {
-	int mult = 0;
+	int multiplication;
 
-	if (argc == 3)
+	if (argc == 3) 
 	{
-		mult = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", mult);
+		multiplication = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", multiplication);
 	}
 	else
 	{
-		printf("Error\n");
-		return (1);
+	printf("Error\n");
+	return (1);
 	}
+
 	return (0);
 }
