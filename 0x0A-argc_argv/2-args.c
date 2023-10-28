5d80e6a272f229a9da02aa3e77928ca9447b96b9
moharafat <mohamedamr0001@gmail.com>
2023-10-28 11:34:31 +0200

    s

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the program name + new line
+ *@argc: int
+ *@argv: string
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+}
