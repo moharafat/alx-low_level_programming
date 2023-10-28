2fab73a7ca2eae8daec212b908212bba20795b1a
moharafat <mohamedamr0001@gmail.com>
2023-10-28 10:45:01 +0200

    s

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,9 +10,9 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for(i = 0; i < argc ; i++)
+	for (i = 0; i < argc ; i++)
 	{
-		printf ("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
