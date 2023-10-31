9f72f6a88a858b7cd0e325cc86d69d34f9ec51ce
moharafat <mohamedamr0001@gmail.com>
2023-10-31 20:45:28 +0200

    s

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,13 +3,13 @@
 /**
  * str_concat - Write a function that concatenates two strings.
  * @s1: the string that is added by the used
- * @s2: the 2nd string to be added to the 1st 
+ * @s2: the 2nd string to be added to the 1st
  * Return: NULL if str = 0
  */
 char *str_concat(char *s1, char *s2)
 {
 	int length = 0;
-	int array_index =0;
+	int array_index = 0;
 	int index;
 	char *array;
 
