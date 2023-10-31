e7aac0fd7a543fc0682eadcc55e5b8e3d4a7e627
moharafat <mohamedamr0001@gmail.com>
2023-10-31 13:04:20 +0200

    s

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *create_array - function that creates an array of chars
+ *  and initializes it with a specific char
+ * @size: the size of the aray
+ * @c:the char user input, array intialization
+ * Return: NULL if size = 0
+ * Returns a pointer to the array, or NULL if it fails
+ */
