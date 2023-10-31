ebbb84cda364789126bbb47b32d08d28fa60d858
moharafat <mohamedamr0001@gmail.com>
2023-10-31 15:01:01 +0200

    s

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,10 +1,32 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * *create_array - function that creates an array of chars
- *  and initializes it with a specific char
- * @size: the size of the aray
- * @c:the char user input, array intialization
- * Return: NULL if size = 0
- * Returns a pointer to the array, or NULL if it fails
+ * _strdup - returns a pointer to a newly allocated space in memory, 
+ * which contains a copy of the string given as a parameter.
+ * @str: the string taht is added by the used
+ * Return: NULL if str = 0
  */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	char *p;
+	unsigned int size; // I presume that that is the index
+
+	p = malloc(sizeof(char) * size);
+	duplicate = _strdup(str);
+	if (str == NULL) //Returns NULL if str = NULL
+	{
+		return (NULL);
+	}
+	if (duplicate > size)//If insufficient memory was available It returns NULL 
+	{						//the missing if 
+		return (NULL);
+	}
+	if (size == 0 )
+	{
+		return (0);
+	}
+	
+
+	return (duplicate); //succsses
+}
