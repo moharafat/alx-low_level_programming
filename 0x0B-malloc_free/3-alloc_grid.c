2a3d94bcad981676186276538abb13f299c563a8
moharafat <mohamedamr0001@gmail.com>
2023-11-01 21:12:57 +0200

    s

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	int **arr;
-	arr = malloc(sizeof(int *) * height);//int* was important
+	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
 	{
 		return (NULL);
