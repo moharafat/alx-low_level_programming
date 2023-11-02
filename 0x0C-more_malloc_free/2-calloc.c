5882ba9b95370fba743006f48ceaf1737bfbb03f
moharafat <mohamedamr0001@gmail.com>
2023-11-02 15:44:38 +0200

    s

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,5 +9,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+    void *ptr;
+    unsigned int i;
+    unsigned int total_size;
 
+    if (nmemb == 0 || size == 0)
+        return (NULL);
+
+    total_size = nmemb * size;
+    ptr = malloc(total_size);
+
+    if (ptr == NULL)
+        return (NULL);
+
+    for (i = 0; i < total_size; i++)
+        ((char *)ptr)[i] = 0;
+
+    return (ptr);
 }
