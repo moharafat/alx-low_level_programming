c082105484fc1faa8920cd959c0b982608367063
moharafat <mohamedamr0001@gmail.com>
2023-11-02 17:11:18 +0200

    s

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -24,7 +24,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	filler = ptr;
 	for (i = 0; i < total_size; i++)
-		ptr[i] = '\0';
+		filler[i] = '\0';
 
 	return (ptr);
 }
