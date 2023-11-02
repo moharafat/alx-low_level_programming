4961fb1cbd229c3fea628e826e35a89414147ab3
moharafat <mohamedamr0001@gmail.com>
2023-11-02 10:52:59 +0200

    s

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,13 +20,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2++;
 	}
 	l = len1 +len2 + 1;
-	s = malloc(l * sinzeof(char));
+	s = malloc(l * sizeof(char));
 	for (i = 0; s1[i] != 0; i++)
 	{
 		s[i] = s1[1];
 	}
 	for (i = 0; s2[i <= n]; i++)
 	{
-		s[len1 + i] = s2[i]
+		s[len1 + i] = s2[i];
 	}
 }
