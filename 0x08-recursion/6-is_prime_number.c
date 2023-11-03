ae7ff6b65964dd7ed630b5176a2a0f3c2b8519e9
moharafat <mohamedamr0001@gmail.com>
2023-11-03 14:10:21 +0200

    s

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,32 @@
 #include "main.h"
-/**
- * is_prime_number - check the code
- *@n: input parameter
- * Return: Always 0.
- */
+#include <math.h> // Include math library for sqrt function
 
+/**
+ * helper - Helper function to check if a number is prime
+ * @n: The number to check for primality
+ * @divisor: The current divisor to check
+ * Return: 1 if prime, 0 if not
+ */
 int helper(int n, int divisor)
 {
-	if (n == divisor)
-	{
-	return (1);
-	}
-	if (n % divisor == 0)
-	{
-	return (0);
-	}
-	if (n == 1)
-	return (0);
+    if (n <= 1)
+        return 0; // 0 and 1 are not prime
 
-	return (helper(n, divisor + 1));
+    if (divisor > sqrt(n))
+        return 1; // All divisors checked, it's prime
+
+    if (n % divisor == 0)
+        return 0; // Found a divisor, it's not prime
+
+    return helper(n, divisor + 1); // Check the next divisor
 }
 
+/**
+ * is_prime_number - Check if a number is prime
+ * @n: The number to check for primality
+ * Return: 1 if prime, 0 if not
+ */
 int is_prime_number(int n)
-
 {
-	int divisor;
-	divisor = 0;
-
-	return helper(n, divisor);
+    return helper(n, 2); // Start checking from divisor 2 (the smallest prime)
 }
