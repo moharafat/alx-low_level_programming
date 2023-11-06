70e83756f29451ce4ca4800f7f7eaa0d3cc359e0
moharafat <mohamedamr0001@gmail.com>
2023-11-07 00:09:32 +0200

    s

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-void init_dog(struct dog *d, char *name, float age, char *owner)
 
-struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    char *name;
-    float age;
-    char *owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 };
 
 #endif
