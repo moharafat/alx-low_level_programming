c29105d233ec1bb1a492cdbde1ba2c39bd712345
moharafat <mohamedamr0001@gmail.com>
2023-11-07 00:49:25 +0200

    s

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("nil");
+	}
+
+	if (d->name == NULL)
+	{
+		printf("nil");
+	}
+
+
+	if (d-> == NULL)
+	{
+		printf("");
+	}
+}
