05bba059d285d32049a158c5e56c694dfdbd26da
moharafat <mohamedamr0001@gmail.com>
2023-11-07 00:26:28 +0200

    s

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
- * File: dog.h
- * Auth: Brennan D Baraban
- * Desc: Header file that defines a new type struct dog.
- */
-
 /**
  * struct dog - A new type describing a dog.
  * @name: The name of the dog.
@@ -20,9 +14,7 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for struct dog
- */
+
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
