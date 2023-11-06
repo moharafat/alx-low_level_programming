4e1771107c663b04d84101a3f0040e7af796fe25
moharafat <mohamedamr0001@gmail.com>
2023-11-06 09:01:29 +0200

    s

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,45 +7,6 @@
  *Return: the pointer to the newly created array or NULL on failure.
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-
 {
-
-    if (new_size == old_size)
-
-        return ptr;
-
-
-    if (new_size == 0)
-
-    {
-
-        free(ptr);
-
-        return NULL;
-
-    }
-
-
-    void *new_ptr = malloc(new_size);
-
-    if (new_ptr == NULL)
-
-        return NULL;
-
-
-    if (ptr != NULL)
-
-    {
-
-        unsigned int copy_size = (old_size < new_size) ? old_size : new_size;
-
-        memcpy(new_ptr, ptr, copy_size);
-
-        free(ptr);
-
-    }
-
-
-    return new_ptr;
 
 }
