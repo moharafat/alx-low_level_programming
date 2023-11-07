2a21c9c6eaf9586bdcdb5b8a826448f85a0b12c4
moharafat <mohamedamr0001@gmail.com>
2023-11-07 05:50:19 +0200

    s

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,13 +4,17 @@
 /**
  * free_dog - Frees dogs.
  * @d: The dog to be freed.
+ * Return: void
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
+	if (d)
+	{
+		if (d->name)
+		free(d->name);
 
-	free(d->owner);
-	free(d->name);
-	free(d);
+		if (d->owner)
+		free(d->owner);
+		free(d);
+	}
 }
