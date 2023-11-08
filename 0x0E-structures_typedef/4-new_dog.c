bc4e6d006c1365bf0b86e8ddf0e5e7686a56a89c
moharafat <mohamedamr0001@gmail.com>
2023-11-08 05:27:38 +0200

    s

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *tmp
+	dog_t *tmp;
 	char* cpname, cpowner;
 	int i, lenname, lenowner;
 
