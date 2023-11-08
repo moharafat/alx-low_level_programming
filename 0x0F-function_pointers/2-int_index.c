5362f60f97f30acf700516f0649071ffab77b7cb
moharafat <mohamedamr0001@gmail.com>
2023-11-08 20:54:42 +0200

    s

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,6 +8,20 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    if (array )
-
+	if (array == NULL || size <= 0 || cmp == NULL)
+	{
+		return (-1);
+	}
+		int i;
+		for (i = 0; i < size; i++)
+		{
+			cmp(array[i]);
+			int result = cmp(array[i]);
+			if (result != 0)
+			{
+				return (i);
+			}
+			
+		}
+		return (-1);
 }
