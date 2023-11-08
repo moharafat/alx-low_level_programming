1259a1f454833c6e3e8e28a124f21bc5f474e447
moharafat <mohamedamr0001@gmail.com>
2023-11-08 18:18:53 +0200

    s

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 /**
  * print_name - Prints a name.
  * @name: The name taht will be printed.
@@ -8,8 +7,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if ((name != NULL) && (f != NULL))
-	{
-		f(name);
-	}
+	if (name && f)
+	f(name);
 }
