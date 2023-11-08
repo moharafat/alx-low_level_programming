7938f9d7ca627f4add7ab8aa950b7ac75b968e46
moharafat <mohamedamr0001@gmail.com>
2023-11-08 19:08:23 +0200

    s

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer.
+ * @array: the integer to print
+ * @size:  number of elements in the array
+ * @cmp:is a pointer to the function you need to use
+ * Return: Nothing.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+}
