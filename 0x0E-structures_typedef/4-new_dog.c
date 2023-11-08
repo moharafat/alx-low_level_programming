c260c656508723a847dba51bef073981a89daaf4
moharafat <mohamedamr0001@gmail.com>
2023-11-08 05:33:46 +0200

    S

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,7 +20,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		if (cpowner == NULL)
 		{
 			free(cpname);
-			return (null);
+			return (NULL);
 		}
 		_strcp(cpowner, owner);
 	tmp = malloc (sizeof (dog_t));
