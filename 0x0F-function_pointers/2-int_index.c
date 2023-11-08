31a885940ed03473c4b304648620ade106c6918b
moharafat <mohamedamr0001@gmail.com>
2023-11-08 19:14:32 +0200

    s

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,10 +3,11 @@
  * int_index - searches for an integer.
  * @array: the integer to print
  * @size:  number of elements in the array
- * @cmp:is a pointer to the function you need to use
- * Return: Nothing.
+ * @cmp: is a pointer to the function to be used to compare values
+ * Return: int
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+    if (array )
 
 }
