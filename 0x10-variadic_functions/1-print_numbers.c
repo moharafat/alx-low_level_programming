316c43796a7905f6dc01151c1dba29831e35cdcd
moharafat <mohamedamr0001@gmail.com>
2023-11-09 17:33:07 +0200

    s

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,6 +2,14 @@
 #include <stdarg.h>
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list ptr;
-    
+	va_list ptr;
+	unsigned int i;
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf(va_arg(ptr, int));
+	}
+			va_end(ptr);
+
 }
