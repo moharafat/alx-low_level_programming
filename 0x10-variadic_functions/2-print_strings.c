01d7c99bdf716d3d8e827273a000c82f97cd9f39
moharafat <mohamedamr0001@gmail.com>
2023-11-09 20:59:27 +0200

    s

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,38 +1,26 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-/**
- * print_strings - function that prints strings, followed by a new line
- * @separator:  is the string to be printed between strings
- * @n: The number of strings passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
- * Return: always zero.
- */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list ptr;
-	unsigned int i;
+  va_list ptr;
+  unsigned int i;
 
-	va_start(ptr, n);
+  va_start(ptr, n);
 
-	
-	for(i = 0; i < n; i++)
-	{
-		const char *str = va_arg(ptr, char *);
+  for (i = 0; i < n; i++) {
+    const char *str = va_arg(ptr, char *);
+    if (str == NULL) {
+      printf("(nil)");
+    } else {
+      printf("%s", str);
+    }
+    if (separator != NULL && i != n - 1) {
+      printf("%s", separator);
+    }
+  }
 
-		if ( str == NULL)
-		{
-			printf("nil");
-		}
-		else
-		{
-		printf("%s", str);
-		}		
-		if (separator != NULL && i != 0)
-		{
-			printf("%s", separator);	
-		}
-	}
-	va_end(ptr);
-	printf("\n");
+  va_end(ptr);
+  printf("\n");
 }
