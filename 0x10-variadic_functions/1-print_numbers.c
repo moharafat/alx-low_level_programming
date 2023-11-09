b1c8514f8adc952fa0559d11c71544eace300e56
moharafat <mohamedamr0001@gmail.com>
2023-11-09 19:49:51 +0200

    s

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,13 +2,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 
- /**
-  * print_numbers - prints numbers given as parameters.
-  * @separator:  is the string to be printed between numbers
-  * @n: The number of paramters passed to the function.
-  * @...: A variable number of paramters to calculate the sum of.
-  * Return: always zero.
-  */
+/**
+ * print_numbers - Prints numbers, followed by a new line.
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ptr;
