bc35ac70032c6b908c64f0316d5c71de4d0e110d
moharafat <mohamedamr0001@gmail.com>
2023-11-09 20:41:31 +0200

    s

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	
 	for(i = 0; i < n; i++)
 	{
-		char *str = va_arg(ptr, char);
+		char *str = va_arg(ptr, char *);
 
 		if ( str == NULL)
 		{
