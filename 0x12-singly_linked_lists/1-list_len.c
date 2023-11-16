b20f333fa1b32a99166a47154b4aefcb35a90b11
moharafat <mohamedamr0001@gmail.com>
2023-11-17 01:21:00 +0200

    s

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,16 +7,13 @@
  */
 size_t list_len(const list_t *h)
 {
-/*we will iterate inside each node counting the elements*/
-/*the pointer h will get us into the next node using h->next */
-	size_t elements = 0;
-	int i;
+    size_t elements = 0;
+    int i;
 
-	for (i = 0; h != NULL ;i++)
-	{
-		elements = elements + h->len;
-		h = h->next;
-		elements++;
-	}
-	return (elements);
+    for (i = 0; h != NULL ;i++)
+    {
+        elements++;
+        h = h->next;
+    }
+    return (elements);
 }
