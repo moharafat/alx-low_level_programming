c77a7a7e20194af9a1ab0e62cb15cbea9125e407
moharafat <mohamedamr0001@gmail.com>
2023-11-17 01:28:09 +0200

    S

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 /**
  * list_len - returns the number of elements in a linked list_t list
  * @h: refers to the first node (head)
- * Return: number of elements 
+ * Return: number of elements
  */
 size_t list_len(const list_t *h)
 {
@@ -12,7 +12,7 @@ size_t list_len(const list_t *h)
 	size_t elements = 0;
 	int i;
 
-	for (i = 0; h != NULL ;i++)
+	for (i = 0; h != NULL ; i++)
 	{
 		h = h->next;
 		elements++;
