b8bed985a2d21ea0df005529f20e528c119f6d85
moharafat <mohamedamr0001@gmail.com>
2023-11-17 00:42:19 +0200

    s

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: refers to the first node (head)
+ * Return: number of elements 
+ */
 size_t list_len(const list_t *h)
+/*we will itirate inside each node counting the elements*/
+/*the pointer h will get us into the next node using h->next */
 {
-    size_t count;
-    
+	size_t elements = 0;
+	for (i = 0; h->next != NULL ;i++)
+	{
+
+	
+	}
+	return (elements);
 }
+
+	count++;
+		h = h->next;	
