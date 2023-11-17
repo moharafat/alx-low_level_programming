ae741852e82d5d7f33eb9003f26120e2038d714f
moharafat <mohamedamr0001@gmail.com>
2023-11-17 03:16:07 +0200

    s

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -33,6 +33,11 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new_node->str =strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return NULL;
+	}
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
