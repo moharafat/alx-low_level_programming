591fca3d2974629f77019418a8c1fa6a709c23c4
moharafat <mohamedamr0001@gmail.com>
2023-11-17 03:10:18 +0200

    s

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,21 @@
 #include "lists.h"
 #include <string.h>
+#include <stdio.h>
+/**
+ * _strlen - function that returns the length of a string
+ *@s: string parameter input
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; *s != '\0'; s++)
+		++length;
+return (length);
+}
+
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: refers to the first node (head)
@@ -15,7 +31,7 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
-	new_node->str =strup(str);
+	new_node->str =strdup(str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
