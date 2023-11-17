7120d752699fbfa6dc2065d7dab69325d1e8a136
moharafat <mohamedamr0001@gmail.com>
2023-11-17 04:00:19 +0200

    s

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * before the main - prints string before main function
+ * before_main - prints string before main function
  */
-void myConstructor( void ) __attribute__ ((constructor))
+void __attribute__((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow,\n"
 	"I bore my house upon my back!\n");
