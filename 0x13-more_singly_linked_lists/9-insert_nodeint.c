55b86373e6c46644bba10d2c3415fcc9f5870529
moharafat <mohamedamr0001@gmail.com>
2023-11-20 18:29:55 +0200

    s

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	/*while(idx != idx - 2)*/
 	while (idx != 1)
 	{
-		ptr = ptr->next
+		ptr = ptr->next;
 		idx--;
 	}
 	new_node->next = ptr->next;
