9ad2ddf914f75b397479007eb8f45e1459cde0c4
moharafat <mohamedamr0001@gmail.com>
2023-11-20 14:18:08 +0200

    s

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,22 +1,19 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 int pop_listint(listint_t **head)
 {
-    if (head == NULL || *head == NULL) 
+	if (head == NULL)
 	{
-        return -1; 
-    } 
-	else {
-        listint_t *temp = *head;
-        int removed_data = temp->n;
-
-        *head = (*head)->next;
-
-        free(temp);
-        temp = NULL;
-
-        return removed_data;
-    }
+		return (NULL);
+	}
+	else
+	{
+	listint_t *temp = *head;
+	head = head->next;
+	free(temp);
+	temp = NULL;
+	}
+	return (head);
 }
