4a71d544a2693f14e0704913f6c19de40826f890
moharafat <mohamedamr0001@gmail.com>
2023-11-20 18:26:40 +0200

    s

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * pop_listint - Deletes the head node of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @idx: is the index of the list where the new node should be added
+ * @n: integer inside  new node 
+ * Return:  the address of the new node, or NULL if it failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *ptr = *head;
+	listint_t *new_node = malloc(sizeof(listint_t));
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 1)
+	{
+		new_node->link = ptr;
+		*head = new_node;
+	}
+	else
+		idx--;
+	?/*while(idx != idx - 2)*/
+	while (idx != 1)
+	{
+		ptr = ptr->next
+		idx--;
+	}
+	new_node->next = ptr->next;
+	ptr->next = new_node;
+}
