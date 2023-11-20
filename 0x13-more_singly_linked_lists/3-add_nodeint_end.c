efbde48eb088c8583d0b3aaebb90f5880aded2eb
moharafat <mohamedamr0001@gmail.com>
2023-11-20 12:11:37 +0200

    s

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,7 +21,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (*head == NULL)
     {
-        *head = new_node; // If the list is empty, new node becomes the head
+        *head = new_node; /* If the list is empty, new node becomes the head*/
         return (new_node);
     }
 	while (temp != NULL)
