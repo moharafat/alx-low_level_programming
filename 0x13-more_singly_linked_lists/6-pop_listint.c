103bdc12dd091e1b992ac78ebefb5cfde031a7c2
moharafat <mohamedamr0001@gmail.com>
2023-11-20 14:12:06 +0200

    s

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,12 +6,12 @@ int pop_listint(listint_t **head)
 {
 	if (head == NULL)
 	{
-		return (NULL);
+		return (-1);
 	}
 	else
 	{
 	listint_t *temp = *head;
-	head = head->next;
+	*head = *head->next;
 	free(temp);
 	temp = NULL;
 	}
