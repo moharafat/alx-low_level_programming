ad9fa67ffddd484d4f8bd7bf03705721e04e253f
moharafat <mohamedamr0001@gmail.com>
2023-11-21 23:45:50 +0200

    s

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 	{
 		return (0);
 	}
