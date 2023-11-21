c30c717568af25ffff160357dd661af6d42cb7f8
moharafat <mohamedamr0001@gmail.com>
2023-11-21 06:17:04 +0200

    s

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -4,7 +4,7 @@
  * insert_nodeint_at_index - iserts a new node at a given position.
  * @head: A pointer to the address of the
  *        head of the listint_t list.
- * @idx: is the index of the list where the new node should be deleted 
+ * @index: is the index of the list where the new node should be deleted
  * Return:  1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
