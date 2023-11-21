bb5281796bc73b59089d086a18910a0684b09067
moharafat <mohamedamr0001@gmail.com>
2023-11-21 06:13:35 +0200

    s

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			index--;
 		}
 	}
-	previous->link = current->link;
+	previous->next = current->next;
 	free(current);
 	current = NULL;
 }
