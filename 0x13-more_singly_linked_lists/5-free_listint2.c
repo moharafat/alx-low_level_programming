28576554c2f521730947321a301834950defca5d
moharafat <mohamedamr0001@gmail.com>
2023-11-22 00:08:20 +0200

    s

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,12 @@ void free_listint2(listint_t **head)
 	listint_t *current_node;
 	listint_t *next_node;
 
-	current_node = head;
+	current_node = *head;
 	while (current_node != NULL)
 	{
 		next_node = current_node->next;
 		free(current_node);
 		current_node = next_node;
 	}
-	head = NULL;
+	*head = NULL;
 }
