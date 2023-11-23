82f0650aed955adb040d8e3739339708e0f055a3
moharafat <mohamedamr0001@gmail.com>
2023-11-23 13:08:05 +0200

    s

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 /**
  * binary_to_uint -  converts a binary number to an unsigned int
  * @b: pointing to a string of 0 and 1 chars
- * Return: converted number or 0 if 
+ * Return: converted number or 0 if
  *  in the string b that is not 0 or 1 or b is NULL
  */
 unsigned int binary_to_uint(const char *b)
@@ -14,9 +14,9 @@ unsigned int binary_to_uint(const char *b)
 	{
 		return (0);
 	}
-	while (*b != '\0') 
+	while (*b != '\0')
 	{
-		if (*b != '0' && *b != '1') 
+		if (*b != '0' && *b != '1')
 		{
 			return (0);
 		}
