fbbb0ed1f44c8c1901878e27254a0276ab089643
moharafat <mohamedamr0001@gmail.com>
2023-11-24 00:16:26 +0200

    s

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,11 +12,11 @@ int get_bit(unsigned long int n, unsigned int index)
 	{
 		return (-1);
 	}
-	else if (n >> index == 1)
+	else if ((n >> index & 1) == 1)
 	{
 	return (1);
 	}
-	else if (n >> index == 0)
+	else if ((n >> index & 0)== 0)
 	{
 	return (0);
 	}
