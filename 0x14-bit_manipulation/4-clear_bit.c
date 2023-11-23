517df100bf66d614626b9688c85ec549645671a4
moharafat <mohamedamr0001@gmail.com>
2023-11-24 01:46:36 +0200

    s

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -16,8 +16,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	{
 		return (1);
 	}
-	*n |= (0 << index);
+	if (*n >> index & 1)
+    {
+    *n |= (0 << index);
 	return (1);
-
+    }
 }
 
