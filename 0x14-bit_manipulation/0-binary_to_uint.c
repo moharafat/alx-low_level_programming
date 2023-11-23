dd9c0d2fe652416393bc102b968c7d079a40fe44
moharafat <mohamedamr0001@gmail.com>
2023-11-23 13:02:53 +0200

    s

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,18 @@
 #include "main.h"
 #include <stdio.h>
-unsigned int binary_to_uint(const char *b) {
+unsigned int binary_to_uint(const char *b) 
+{
 	unsigned int sum = 0;
-    if (b == NULL) {
-        return 0;
-    }
-
-
-
-    while (*b != '\0') {
-        if (*b != '0' && *b != '1') {
-            return 0;
-        }
-        
-        sum = sum * 2 + (*b - '0');
-        b++;
-    }
-
-    return sum;
+	if (b == NULL) 
+	{
+		return 0;
+	}
+	while (*b != '\0') {
+		if (*b != '0' && *b != '1') {
+			return 0;
+		}
+		sum = sum * 2 + (*b - '0');
+		b++;
+	}
+	return sum;
 }
