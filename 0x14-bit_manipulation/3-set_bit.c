9adee2e6f3849e90c44a67ec645ff36fcc7805ac
moharafat <mohamedamr0001@gmail.com>
2023-11-24 00:57:04 +0200

    x

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,7 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	}
 	if (*n >> index & 1 == 0)
 	{
-		*n | (1 << index);
+		*n |= (1 << index);
 		return (1);
 	}
 	if (*n >> index & 1 == 1)
