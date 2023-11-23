ba1297c4c310da1baaea125426e0453d36f3abfb
moharafat <mohamedamr0001@gmail.com>
2023-11-24 01:07:17 +0200

    s

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,15 +7,14 @@
  * Return: 1 if it worked, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index) {
-    if (index >= sizeof(unsigned long int) * 8) {
-        return (-1);
-    }
-
-    if ((*n >> index & 1) == 1) {
-        return (1);
-    }
-
-    *n |= (1 << index);
-
-    return (1);
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	if ((*n >> index & 1) == 1) 
+	{
+		return (1);
+	}
+	*n |= (1 << index);
+	return (1);
 }
