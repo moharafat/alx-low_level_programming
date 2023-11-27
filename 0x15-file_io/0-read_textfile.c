9514abd2a4796a7c3e5e5da590ca1fd333cf22b9
moharafat <mohamedamr0001@gmail.com>
2023-11-27 23:25:50 +0200

    s

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	buff = malloc(sizeof(char) * letters);
+	buff = malloc(sizeof(size_t) * letters);
 	if (buff == NULL)
 	{
 		return (0);
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,9 +11,11 @@ int create_file(const char *filename, char *text_content)
 {
 	int fp;
 
-	fd = open(filename, O_CREAT, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC, 0600);
 	if (fp == -1)
 	{
 		return (0);
 	}
+	write(1, text_content, n);
+	close(fd);
 }
