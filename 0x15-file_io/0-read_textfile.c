f87684243b9be035f35f6b309a548a206990658e
moharafat <mohamedamr0001@gmail.com>
2023-11-27 13:07:20 +0200

    s

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * read_textfile -reads a text file and prints it to the POSIX standard output.
+ * @filename: The number whose bit value is to be fliped
+ * @letters: number of letters it should read and print
+ * Return: number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+	int fp;
+	ssize_t bytes_read;
+	char buff[] = letters;
+	fp = open(filename, O_RDONLY | O_RDWR , 0644)
+	if	(fp == -1)
+	{
+		return (0);
+	}
+	read (fp, buff, letters)
+	if (write(buff, sizeof (buff)-1 ) == -1)
+	{
+		exit (0);
+	}
+	if (close (fp) == -1)
+	{
+		exit (0);
+	}
+}
