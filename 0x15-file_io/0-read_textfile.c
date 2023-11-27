052d2966dac88d70070239bc4dd7dca85a81b790
moharafat <mohamedamr0001@gmail.com>
2023-11-27 14:34:07 +0200

    s

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,39 +4,46 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stddef.h>
-/**
- * read_textfile -reads a text file and prints it to the POSIX standard output.
- * @filename: The number whose bit value is to be fliped
- * @letters: number of letters it should read and print
- * Return: number of letters it could read and print
- */
+
 ssize_t read_textfile(const char *filename, size_t letters);
 {
-	int fp;
-	ssize_t bytes_read;
-	char *buff;
-	int n;
-	int *ptr;
+    int fd;
+    ssize_t bytes_read;
+    char *buffer;
+    int n;
+    int *ptr;
 
-	buff = malloc(sizeof (size_t)* letters);
-	if (buff == NULL)
-	{
-		return (0);
-	}
-	fp = open(filename, O_RDONLY | O_RDWR)
-	if	(fp == -1)
-	{
-		return (0);
-	}
-	n = read (fp, buff, letters)
-	if (n == -1)
-	{
-		return (0);
-	}
-	 write(1, buff, n)
-	free(buff);
-	if (close (fp) == -1)
-	{
-		exit (0);
-	}
+    if (filename == NULL) {
+        return (0);
+    }
+
+    buffer = malloc(sizeof(size_t) * letters);
+    if (buffer == NULL) {
+        return (0);
+    }
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        free(buffer);
+        return (0);
+    }
+
+    n = read(fd, buffer, letters);
+    if (n == -1) {
+        close(fd);
+        free(buffer);
+        return (0);
+    }
+
+    bytes_read = write(STDOUT_FILENO, buffer, n);
+    if (bytes_read == -1) {
+        close(fd);
+        free(buffer);
+        return (0);
+    }
+
+    free(buffer);
+    close(fd);
+
+    return (bytes_read);
 }
