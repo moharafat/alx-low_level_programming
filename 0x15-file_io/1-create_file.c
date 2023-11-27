979a6b21e2561353248b44f1d0a71a9710f37466
moharafat <mohamedamr0001@gmail.com>
2023-11-28 00:03:48 +0200

    S

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,19 +3,28 @@
 
 /**
  * create_file - function that creates a file.
- * @filename: is the content inside the file
- * @text_content: number of letters it should read and print
+ * @filename: A pointer to the name of the file to create
+ * @text_content:  A pointer to a string to write to the file.
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fp;
+	int fd;
+	int len = _strlen(text_content);
 
-	fd = open(filename, O_CREAT | O_TRUNC, 0600);
-	if (fp == -1)
+	fd = open(filename, O_WRONLY O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 	{
-		return (0);
+		return (-1);
 	}
-	write(1, text_content, n);
+	if (fielname == NULL)
+	{
+		return (-1);
+	}
+	while (len != NULL)
+	{
+	write(1, text_content, len);
+	}
 	close(fd);
+	return (1);
 }
