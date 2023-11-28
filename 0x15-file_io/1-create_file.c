b10b3accaf2ea48c4583a4296c9e72e7f4cd7309
moharafat <mohamedamr0001@gmail.com>
2023-11-28 18:47:40 +0200

    s

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,8 +9,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, n;
 	n = 0;
-	int len;
-	len = strlen(text_content);
+
 	if (filename == NULL)
 	{
 		return (-1);
@@ -22,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		n = write(fd, text_content, len);
+		n = write(fd, text_content, strlen(text_content));
 		if (n == -1)
 		{
 			close(fd);
