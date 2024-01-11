b83e3ceaa30fa7e586eb2dc9096d3821016acd82
moharafat <mohamedamr0001@gmail.com>
2024-01-11 17:42:48 +0200

    S

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_dlistint -frees a dlistint_t list.
+ * @head: A pointer to the address of the head of the dlistint_t list.
+ * Return:0
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *currnet_node;
+	dlistint_t *next_node;
+
+	currnet_node = head;
+	next_node = head;
+	while (currnet_node != NULL)
+	{
+		next_node = currnet_node->next;
+		free(currnet_node);
+		currnet_node = next_node;
+	}
+	head = NULL;
+}
