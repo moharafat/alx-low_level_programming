a620721434eb95cc89acd43fa23f1f783317ae8d
moharafat <mohamedamr0001@gmail.com>
2024-01-11 20:55:54 +0200

    S

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,6 +23,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_node->next = ptr;
 		new_node->prev = NULL;
+		if (ptr != NULL)
+			ptr->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
