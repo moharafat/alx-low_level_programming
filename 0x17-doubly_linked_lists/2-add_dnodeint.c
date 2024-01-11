346d8d38b874da8704b645cd0ae11e0481f89317
moharafat <mohamedamr0001@gmail.com>
2024-01-11 15:39:52 +0200

    s

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,9 +14,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = *head;
-	new_node->prev = NULL;
-	*head = new_node;
+	else
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		new_node->prev = NULL;
+		*head = new_node;
+	}
 	return (new_node);
 }
