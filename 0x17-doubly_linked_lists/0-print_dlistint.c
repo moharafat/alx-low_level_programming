4cee743502d418df27f630739eb94f55e03a5843
moharafat <mohamedamr0001@gmail.com>
2024-01-11 13:39:48 +0200

    s

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,7 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  *  print_dlistint -  prints all the elements of a dlistint_t list.
  * @h: refers to the first node (head)
