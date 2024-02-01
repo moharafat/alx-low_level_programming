7645587f84d7acaed88d087618af0c55a7f19a59
moharafat <mohamedamr0001@gmail.com>
2024-02-02 01:36:27 +0200

    s

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t));
+	table->array = calloc(table->size, sizeof(hash_node_t*));
 	for (i = 0; i < table->size; i++)
 	{
 		table->array[i] = NULL;
