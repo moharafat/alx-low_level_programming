43c76a5980fd0c06f8c63d612cc0a1068b975b0f
moharafat <mohamedamr0001@gmail.com>
2024-02-02 19:48:35 +0200

    adding at the begining

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,8 @@
 #include "hash_tables.h"
 /**
  * key_index - function that gives you the index of a key.
- * @size: the size of the array
+ * @key: is the key
+ * @size: size of the array of the hash table
  * Return: index at which the key/value pair should be stored
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
