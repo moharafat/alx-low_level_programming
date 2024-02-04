d3dac90bdb78038b2b6084b76cde292e111eed85
moharafat <mohamedamr0001@gmail.com>
2024-02-04 13:10:29 +0200

    freeing

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - function that deletes a hash table.
+ * @ht: is the hash table
+ * Return: 0
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current_item;
+
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current_item =  ht->array[i];
+		while (current_item != NULL)
+		{
+			free(current_item->key);
+			free(current_item->value);
+			current_item = current_item->next;
+		}
+	}
+	free(ht->array);
+}
